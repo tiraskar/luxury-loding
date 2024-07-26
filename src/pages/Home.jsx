79a685c7f577d32a974c9ac287336c48bccf845f
tribikram <sentribikram@gmail.com>
2024-07-26 14:24:43 +0545

    question and answer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import ActivitiesAndServices from "./components/ActivitiesAndServices";
 import Banner from "./components/Banner";
+import QuestionAnswer from "./components/QnA";
 
 const Home = () => {
   return (
     <div className="space-y-20 font-onest">
       <Banner />
       <ActivitiesAndServices />
+      <QuestionAnswer />
     </div>
   );
 };
