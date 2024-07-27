88d56ae5f38f3d2b64b068f28fb58b889e8c3851
tribikram <sentribikram@gmail.com>
2024-07-28 00:28:08 +0545

    add card inside each image in accommodations

--- src/pages/components/Accommodations.jsx
+++ src/pages/components/Accommodations.jsx
@@ -16,21 +16,37 @@ const Accommodations = () => {
       <div className="flex flex-col gap-1 max-w-[1536px] mx-auto justify-center rounded-2xl py-[56px]">
         <div className="relative">
           <img src="/images/accommodation-one.png" alt="" />
-          <div className="absolute">
-            <h1>Hello</h1>
+          <div className="font-onest absolute flex flex-col bg-cardBackgroundDark bottom-8 right-8 gap-y-10  p-5 rounded-2xl">
+            <h1 className="text-2xl leading-9 tracking-tight max-w-[148px]">
+              Best price guarentee
+            </h1>
+            <p className="text-sm leading-[22px] max-w-[298px]">
+              Our best price guarantee ensures you get the most competitive rates, without any extra booking fees or commissions, when you book directly with us.
+            </p>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-1">
+        <div className="relative grid grid-cols-2 gap-1">
           <div className="relative">
             <img src="/images/accommodation-two.png" alt="" />
-            <div className="absolute bg-cardBackgroundDark p-2">
-              <h1>Hello</h1>
+
+            <div className="font-onest absolute flex flex-col bg-cardBackgroundDark bottom-8 right-8 gap-y-10  p-5 rounded-2xl">
+              <h1 className="text-2xl leading-9 tracking-tight max-w-[265px]">
+                Exclusive deals and promotions
+              </h1>
+              <p className="text-sm leading-[22px] max-w-[298px]">
+                Unlock exclusive deals and promotions available only to our direct guests, offering extra value and special offers tailored for your stay.
+              </p>
             </div>
           </div>
           <div className="relative">
             <img src="/images/accommodation-three.png" alt="" />
-            <div className="absolute">
-              <h1>Hello</h1>
+            <div className="font-onest absolute flex flex-col bg-cardBackgroundDark bottom-8 right-8 gap-y-10  p-5 rounded-2xl">
+              <h1 className="text-2xl leading-9 tracking-tight max-w-[156px]">
+                Personalized service
+              </h1>
+              <p className="text-sm leading-[22px] max-w-[298px]">
+                Experience personalized service when booking directly with us. Our dedicated team is committed to tailoring your stay to your unique preferences, ensuring exceptional hospitality.
+              </p>
             </div>
           </div>
         </div>
