2179a564b2910ca6ceac26e7a9b5edaea4732e7b
tribikram <sentribikram@gmail.com>
2024-07-27 10:04:09 +0545

    guide book single view responsive design

--- src/pages/BlogAndGuideBook.jsx
+++ src/pages/BlogAndGuideBook.jsx
@@ -1,9 +1,11 @@
+import GuideBookBanner from "./components/GuideBookBanner";
 import LatestPosts from "./components/LatestPosts";
 import RentalExperience from "./components/RentalExperience";
 
 const BlogAndGuideBook = () => {
   return (
     <div className="flex flex-col gap-y-[150px]">
+      <GuideBookBanner />
       <LatestPosts />
       <RentalExperience />
     </div>
