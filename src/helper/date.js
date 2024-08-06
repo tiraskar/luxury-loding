add0d702075f075224353ac6dfeb3fb2f6869a48
tribikram <sentribikram@gmail.com>
2024-08-06 18:46:19 +0545

    formate date

---
+++ src/helper/date.js
@@ -0,0 +1,11 @@
+export const formateDate = (date) => {
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    return '';
+  }
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
