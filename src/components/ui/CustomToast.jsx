92dab4716f46a8b329dc7241ee2cb4f6856d67aa
tribikram <sentribikram@gmail.com>
2024-08-07 20:36:42 +0545

    handle custom toast message

--- src/components/ui/CustomToast.jsx
+++ src/components/ui/CustomToast.jsx
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
 import { toast } from 'react-hot-toast';
-import { RxCross2 } from 'react-icons/rx';
 
 const CustomToast = ({ message, onClose }) => {
   return (
-    <div className="flex items-center max-w-md w-full border-[0.6px] border-[#D7DBE8] rounded-lg ring-1 ring-opacity-40 font-onest">
+    <div
+      className={` ease-out max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 font-onest tracking-tight`}
+    >
       <div className="flex-1 w-0 p-4">
         <div className="flex items-start">
-          <div className="ml-3 flex-1">
-            <p className="text-sm font-medium text-black">{message}</p>
-          </div>
+          <p className="mt-1 text-sm text-black">
+            {message}
+          </p>
         </div>
       </div>
-      <div className="flex">
+      <div onClick={onClose} className="flex border-gray-200">
         <button
-          onClick={onClose}
-          className="w-full rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-[#eb0606]"
+
+          className="w-full rounded-none p-4 flex items-center justify-center text-sm font-medium text-[#e33d01cd]"
         >
-          <RxCross2 size={16} />
+          Close
         </button>
       </div>
     </div>
@@ -31,12 +32,13 @@ CustomToast.propTypes = {
 
 // Usage example
 const notifyToastMessage = (message) => {
-  toast.custom((t) => (
+  toast.dismiss(); // Dismiss any existing toasts to avoid stacking them up
+  toast.custom(() => (
     <CustomToast
       message={message}
-      onClose={() => toast.dismiss(t.id)}
+      onClose={() => toast.dismiss()}
     />
-  ), { duration: 2000 });
+  ), { duration: 1000, position: 'top-center' });
 };
 
 export default CustomToast;
