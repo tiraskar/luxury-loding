db2be91244cb83562a69734154d8354c4c6085f2
tribikram <sentribikram@gmail.com>
2024-08-10 07:16:25 +0545

    add calander for availability

--- src/components/listing/ListingAvailability.jsx
+++ src/components/listing/ListingAvailability.jsx
@@ -1,8 +1,35 @@
 
+import Calendar from "react-calendar";
+
 const ListingAvailability = () => {
+
+  const futureDate = () => {
+    const today = new Date();
+    const futureDate = new Date(today.setMonth(today.getMonth() + 1));
+    return futureDate;
+  };
+
+
   return (
     <div id="listing-availability" className="lg:max-w-[652px] space-y-8 font-inter text-[#333333] tracking-tight">
       <h1 className="text-xl font-semibold">Availability</h1>
+      <div className="flex space-x-3 border-black shadow-md px-[2px] rounded-2xl overflow-hidden font-inter text-textDark text-sm">
+        <Calendar
+          defaultView="month"
+          maxDetail="month"
+          minDate={new Date()}
+          className={''}
+        />
+        <Calendar
+          defaultView="month"
+          maxDetail="month"
+          activeStartDate={futureDate()}
+          className={''}
+        />
+
+      </div>
+      {/* <CustomCalendar /> */}
+
     </div>
   );
 };
