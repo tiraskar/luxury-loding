4ecfd21360fce0aa15f3037a708d1c4acc897b8a
tribikram <sentribikram@gmail.com>
2024-08-16 07:11:49 +0545

    disable load more button when fetching api

--- src/components/ui/LoadMoreListingButton.jsx
+++ src/components/ui/LoadMoreListingButton.jsx
@@ -12,6 +12,7 @@ const LoadMoreListingButton = () => {
       <button
         onClick={() => dispatch(loadMoreListing())}
         className="flex flex-row justify-center items-center tracking-normal font-inter text-[13px] gap-x-2 py-2 px-[10px] border-[0.6px] border-[#D7DBE8] w-fit rounded-2xl"
+        disabled={isLoadMoreListing}
       >
         Load more
         <GrPowerCycle
