d743c8d8e938c9f8373b805bacf9ab1aa4ecf29c
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2024-08-17 11:52:26 +0545

    fixed calendar issue

--- src/components/listing/ListingAvailability.jsx
+++ src/components/listing/ListingAvailability.jsx
@@ -1,7 +1,24 @@
-
+import axios from "axios";
 import Calendar from "react-calendar";
+import { baseUrl } from "../../config/baseurl";
+import { useEffect, useState } from "react";
+import { formateDate } from "../../helper/date";
 
 const ListingAvailability = () => {
+  const [availabilityData, setAvailabilityData] = useState([]);
+  const [currentStartDate, setCurrentStartDate] = useState(new Date());
+  const [nextStartDate, setNextStartDate] = useState(new Date());
+
+  const fetchData = async () => {
+    const response = await axios.get(`${baseUrl}/listing/getcalendar/169542?startDate=2024-08-01`);
+    setAvailabilityData(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+    const date = futureDate();
+    setNextStartDate(date);
+  }, [])
 
   const futureDate = () => {
     const today = new Date();
@@ -9,27 +26,51 @@ const ListingAvailability = () => {
     return futureDate;
   };
 
+  const isDateAvailable = (date) => {
+    const formattedDate = formateDate(date);
+    console.log(formattedDate);
+    const availability = availabilityData.find(
+      (item) => item.date === formattedDate
+    );
+    if (availability && availability.isAvailable === 1) return true;
+    if (availability && availability.isAvailable === 0) return false;
+    if (!availability) return true;
+  };
+
+  const tileDisabled = ({ date }) => {
+    console.log(date);
+    return !isDateAvailable(date);
+  };
 
   return (
     <div id="listing-availability" className="lg:max-w-[652px] space-y-8 font-inter text-[#333333] tracking-tight">
       <h1 className="text-xl font-semibold">Availability</h1>
       <div className="flex space-x-3 border-black shadow-md px-[2px] rounded-2xl overflow-hidden font-inter text-textDark text-sm">
-        <Calendar
-          defaultView="month"
-          maxDetail="month"
-          minDate={new Date()}
-          className={''}
-        />
-        <Calendar
-          defaultView="month"
-          maxDetail="month"
-          activeStartDate={futureDate()}
-          className={''}
-        />
+
+        {availabilityData.length > 0 && (
+          <>
+            <Calendar
+              defaultView="month"
+              maxDetail="month"
+              minDate={new Date()}
+              tileDisabled={tileDisabled}
+              className={''}
+            />
+            <Calendar
+              defaultView="month"
+              maxDetail="month"
+              minDate={futureDate()}
+              activeStartDate={nextStartDate}
+              onActiveStartDateChange={({ activeStartDate }) =>
+                setNextStartDate(activeStartDate)
+              }
+              tileDisabled={tileDisabled}
+              className={''}
+            />
+          </>
+        )}
 
       </div>
-      {/* <CustomCalendar /> */}
-
     </div>
   );
 };
