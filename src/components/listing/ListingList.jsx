35eaadbafedb1ea9b6a42bada6d4ae257d0669e3
tribikram <sentribikram@gmail.com>
2024-08-18 16:49:53 +0545

    state manage for search fetarue from multiple place

--- src/components/listing/ListingList.jsx
+++ src/components/listing/ListingList.jsx
@@ -9,7 +9,7 @@ import LoadMoreListingButton from "../ui/LoadMoreListingButton";
 
 const ListingList = () => {
 
-  const { listingList, listingCount, isFetchListing, isFetchAvailableListing, availableListing, isSearchedListing } =
+  const { listingList, listingCount, isFetchListing, isFetchAvailableListing, availableListing, isSearchedListing, isHomePageSearch } =
     useSelector((state) => state.listing);
 
   const [isMapViewOpen, setIsMapViewOpen] = useState(false);
@@ -20,7 +20,7 @@ const ListingList = () => {
       <div className="text-[#333333] font-onest tracking-tight flex flex-col justify-center gap-y-12 sm:gap-y-[56px]">
         <div className="flex flex-wrap justify-between gap-2">
           <h1 className=" text-3xl sm:text-[35px] font-semibold">
-            {isSearchedListing ? `Available Listings - (${availableListing.length})` : `All Listings - (${listingCount})`}  
+            {isSearchedListing ? `Available Listings - (${availableListing.length})` : `All Listings - (${listingCount || 0})`}  
           </h1>
 
           <ListingViewOptions
@@ -28,41 +28,19 @@ const ListingList = () => {
             setIsMapViewOpen={setIsMapViewOpen}
           />
         </div>
-        {
-          (isFetchListing || isFetchAvailableListing) ? <ListingLoading numbers={8} /> :
-        <RenderListings
-              listingList={isSearchedListing ? availableListing : listingList}
-        />
+        {(isFetchListing || isFetchAvailableListing) ?
+          <ListingLoading numbers={8} />
+          :
+          <RenderListings
+            listingList={(isSearchedListing && !isHomePageSearch) ? availableListing : listingList}
+          />
         }
 
         <div className="flex justify-center">
           {!isSearchedListing && listingCount > listingList.length && <LoadMoreListingButton />
-            //   (
-            //   <button
-            //     onClick={() => dispatch(loadMoreListing())}
-            //     className="flex flex-row justify-center items-center tracking-normal font-inter text-[13px] gap-x-2 py-2 px-[10px] border-[0.6px] border-[#D7DBE8] w-fit rounded-2xl"
-            //   >
-            //     Load more
-            //     <GrPowerCycle
-            //       size={18}
-            //     className={`${isLoadMoreListing && "animate-spin"}`}
-            //     />
-            //   </button>
-            // )
           }
-          {!isSearchedListing && availableListing.length > 8 && <LoadMoreListingButton />
-            //   (
-            //   <button
-            //     onClick={() => dispatch(loadMoreListing())}
-            //     className="flex flex-row justify-center items-center tracking-normal font-inter text-[13px] gap-x-2 py-2 px-[10px] border-[0.6px] border-[#D7DBE8] w-fit rounded-2xl"
-            //   >
-            //     Load more
-            //     <GrPowerCycle
-            //       size={18}
-            //       className={`${isLoadMoreListing && "animate-spin"}`}
-            //     />
-            //   </button>
-            // )
+          {!isSearchedListing && availableListing.length > 8 &&
+            <LoadMoreListingButton />
           }
         </div>
         {/* <ListingMap /> */}
