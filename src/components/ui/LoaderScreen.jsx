9e0cedffd30b39b5113886bbcd522f77d5b3490e
tribikram <sentribikram@gmail.com>
2024-08-18 16:50:47 +0545

    new loader for full screen

--- src/components/ui/LoaderScreen.jsx
+++ src/components/ui/LoaderScreen.jsx
@@ -2,13 +2,35 @@
 
 const LoaderScreen = () => {
   return (
+    // <div className="fixed  bg-black inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+    //   <div className="w-24 h-24 relative">
+    //     <span className="absolute inset-0 w-full h-full border-8 border-dotted  border-white border-opacity-100 rounded-full animate-spin "
+    //     ></span>
+    //   </div>
+    // </div>
+    // <div className="loading-wave">
+    //   <div className="loading-bar"></div>
+    //   <div className="loading-bar"></div>
+    //   <div className="loading-bar"></div>
+    //   <div className="loading-bar"></div>
+    // </div>
     <div className="fixed  bg-black inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-      <div className="w-24 h-24 relative">
-        <span className="absolute inset-0 w-full h-full border-8 border-dotted  border-white border-opacity-100 rounded-full animate-spin "
-        ></span>
+      {/* <div className="w-[300px] h-[100px] flex justify-center items-end">
+        <div className="w-[20px] h-[10px] mx-[5px] bg-buttonPrimary rounded-[5px] animate-loading-wave"></div>
+        <div className="w-[20px] h-[10px] mx-[5px] bg-buttonPrimary rounded-[5px] animate-loading-wave delay-[0.1s]"></div>
+        <div className="w-[20px] h-[10px] mx-[5px] bg-buttonPrimary rounded-[5px] animate-loading-wave delay-[0.2s]"></div>
+        <div className="w-[20px] h-[10px] mx-[5px] bg-buttonPrimary rounded-[5px] animate-loading-wave delay-[0.3s]"></div>
+      </div> */}
+      <div className="loading-wave">
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-bar"></div>
       </div>
     </div>
   );
 };
 
 export default LoaderScreen;
+
+
