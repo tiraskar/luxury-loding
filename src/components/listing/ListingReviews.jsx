23e12b20b6803b198da68597ae53419a2fd3da87
tribikram <sentribikram@gmail.com>
2024-08-18 18:51:13 +0545

    delete useref

--- src/components/listing/ListingReviews.jsx
+++ src/components/listing/ListingReviews.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { GrPowerCycle } from "react-icons/gr";
 import { useDispatch, useSelector } from "react-redux";
