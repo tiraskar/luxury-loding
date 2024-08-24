97ebd436086c1a2b08c17b30d8017c63e13d77e1
tribikram <sentribikram@gmail.com>
2024-08-24 10:34:20 +0545

    fix: check and checkout date input and toast message

--- src/components/listing/SearchListingForm.jsx
+++ src/components/listing/SearchListingForm.jsx
@@ -2,9 +2,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { searchListing, setSearchListingParams, toggleIsSearchedOnSingleListing } from "../../redux/slices/listingSlice";
 import DatePicker from "react-datepicker";
 import LoadingSpinner from "../ui/LoadingSpinner";
-import { notifyToastMessage } from "../ui/CustomToast";
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { toast } from "react-toastify";
 
 const SearchInputLabel = ({ text, htmlFor }) => {
   return (
@@ -41,8 +41,9 @@ const SearchListingForm = () => {
 
   const handleSearch = (e) => {
     e.preventDefault();
+    toast.dismiss();
     if ((searchListingParams.checkIn && !searchListingParams.checkOut) || (searchListingParams.checkOut && !searchListingParams.checkIn)) {
-      return notifyToastMessage("Provide check-in and check-out date.");
+      return toast.info("Provide check-in and check-out date.");
     }
     dispatch(searchListing());
     dispatch(toggleIsSearchedOnSingleListing(true))
@@ -77,16 +78,36 @@ const SearchListingForm = () => {
           />
           <DatePicker type="text"
             selected={searchListingParams.checkIn}
-            onChange={(date) => handleInputChange('checkIn', date)}
-            dateFormat="dd.MM.YYYY"
+            // onChange={(date) => handleInputChange('checkIn', date)}
+            dateFormat="MM.dd.YYYY"
             placeholderText="MM.DD.YYYY"
             minDate={minDateCheckIn}
-            onKeyDown={(e) => {
-              if (e.key !== 'Backspace' && e.key !== 'Delete') {
-                e.preventDefault();
+            className="outline-none w-[101px]  text-[1rem] tracking-[-0.16px] font-inter  h-[19px]"
+            onChange={(date) => {
+              if (date && date instanceof Date && !isNaN(date)) {
+                const currentYear = new Date().getFullYear();
+                if (date.getFullYear() >= currentYear) {
+                  handleInputChange('checkIn', date);
+                } else {
+                  handleInputChange('checkIn', "");
+                }
+              } else {
+                handleInputChange('checkIn', "");
               }
             }}
-            className="outline-none w-[101px]  text-[1rem] tracking-[-0.16px] font-inter  h-[19px]"
+            onBlur={(e) => {
+              const date = new Date(e.target.value);
+              if (date && date instanceof Date && !isNaN(date)) {
+                const currentYear = new Date().getFullYear();
+                if (date.getFullYear() >= currentYear) {
+                  handleInputChange('checkIn', date);
+                } else {
+                  handleInputChange('checkIn', "");
+                }
+              } else {
+                handleInputChange('checkIn', null);
+              }
+            }}
           />
         </div>
 
@@ -99,16 +120,36 @@ const SearchListingForm = () => {
           />
           <DatePicker type="text"
             selected={searchListingParams.checkOut}
-            onChange={(date) => handleInputChange('checkOut', date)}
-            dateFormat="dd.MM.YYYY"
+            // onChange={(date) => handleInputChange('checkOut', date)}
+            dateFormat="MM.dd.YYYY"
             placeholderText="MM.DD.YYYY"
             minDate={minDateCheckOut}
-            onKeyDown={(e) => {
-              if (e.key !== 'Backspace' && e.key !== 'Delete') {
-                e.preventDefault();
+            className="outline-none  w-[101px]  text-[1rem] tracking-[-0.16px] font-inter  h-[19px]"
+            onChange={(date) => {
+              if (date && date instanceof Date && !isNaN(date)) {
+                const currentYear = new Date().getFullYear();
+                if (date.getFullYear() >= currentYear) {
+                  handleInputChange('checkOut', date);
+                } else {
+                  handleInputChange('checkOut', "");
+                }
+              } else {
+                handleInputChange('checkOut', "");
               }
             }}
-            className="outline-none  w-[101px]  text-[1rem] tracking-[-0.16px] font-inter  h-[19px]"
+            onBlur={(e) => {
+              const date = new Date(e.target.value);
+              if (date && date instanceof Date && !isNaN(date)) {
+                const currentYear = new Date().getFullYear();
+                if (date.getFullYear() >= currentYear) {
+                  handleInputChange('checkOut', date);
+                } else {
+                  handleInputChange('checkOut', "");
+                }
+              } else {
+                handleInputChange('checkOut', null);
+              }
+            }}
           />
         </div>
 
@@ -143,8 +184,6 @@ const SearchListingForm = () => {
         className="block lg:hidden  text-white bg-buttonPrimary rounded-xl px-8 py-2 lg:py-4 lg:mt-0 w-full justify-center items-center h-[43px]">
         {isHomePageLoading ? <LoadingSpinner /> : "Search"}
       </button>
-
-
     </div >
   );
 };
