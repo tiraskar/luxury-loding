a46c7e910ea22b08ce0ab916f53caaaf84809146
tribikram <sentribikram@gmail.com>
2024-08-24 18:57:10 +0545

    fix: padding in large screen

--- src/components/common/Wrapper.jsx
+++ src/components/common/Wrapper.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Wrapper = ({ children }) => {
   return (
-    <div className=" items-center max-w-[1320px] 2xl:max-w-[1440px] mx-auto px-2 sm:px-3 md:px-4 xl:px-0">
+    <div className=" items-center max-w-[1320px] 2xl:max-w-[1440px] mx-auto px-2 sm:px-3 md:px-4  xl:px-0">
       {children}
     </div>
   );
