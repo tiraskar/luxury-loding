d350357d30fd008873e2fefb093b9079579fd34a
tribikram <sentribikram@gmail.com>
2024-08-25 09:35:17 +0545

    fetch amenities on listing page

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { FilterableSearchListing, ListingList } from "../components";
 import { useEffect } from "react";
-import { fetchListingList, fetchListingTotalCount, setSearchListingParamsToInitialState } from "../redux/slices/listingSlice";
+import { fetchAmenitiesList, fetchListingList, fetchListingTotalCount, setSearchListingParamsToInitialState } from "../redux/slices/listingSlice";
 
 const Listings = () => {
 
@@ -13,6 +13,8 @@ const Listings = () => {
     listingTotalCount == 0 && dispatch(fetchListingTotalCount());
     listingList.length == 0 && dispatch(fetchListingList());
     dispatch(setSearchListingParamsToInitialState());
+    dispatch(fetchAmenitiesList());
+    localStorage.clear();
   }, []);
 
   return (
