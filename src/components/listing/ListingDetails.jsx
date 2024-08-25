b05f5c5b6eedcfaf737c58f2ae5461a11848b671
tribikram <sentribikram@gmail.com>
2024-08-25 09:34:20 +0545

    add skeleton on loading state

--- src/components/listing/ListingDetails.jsx
+++ src/components/listing/ListingDetails.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import AmenitiesSkeleton from "../ui/AmenitiesSkeleton";
+import { useSelector } from "react-redux";
 
 const ListingDetails = ({ listingAmenities }) => {
 
   const [amenitiesSlice, setAmenitiesSlice] = useState(8)
+  const { isFetchingAmenities } = useSelector(state => state.listing)
 
   return (
     <div id="listing-details" className="tracking-[-1%] space-y-8">
       <h1 className="text-xl font-semibold tracking-[-2%]">Details</h1>
       <div className="grid sm:grid-cols-2 gap-3 ">
-        {listingAmenities?.slice(0, amenitiesSlice).map((amenities) => {
+
+        {isFetchingAmenities && Array.from({ length: 8 }, (_, index) => (
+          <AmenitiesSkeleton key={index} />
+        ))}
+
+        {!isFetchingAmenities && listingAmenities?.slice(0, amenitiesSlice).map((amenities) => {
           return (
             <div key={amenities.amenityId} className="lg:max-w-[318px] flex flex-row items-center p-3 rounded-2xl bg-[#F9F9F9] space-x-3">
               <div className="flex flex-row items-center justify-center h-[42px] w-[42px] rounded-xl bg-white">
