f257765d04768e1d822f6fdf9319808e89c08d57
tribikram <sentribikram@gmail.com>
2024-08-26 23:49:08 +0545

    base url mange from env file

--- src/config/baseurl.js
+++ src/config/baseurl.js
@@ -1,2 +1,5 @@
 
-export const baseUrl = "https://3.136.79.39/luxury_lodging_api";
+// export const baseUrl = "https://3.136.79.39/luxury_lodging_api";
+
+
+export const baseUrl = import.meta.env.VITE_API_URL;
