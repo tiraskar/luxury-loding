b012dfdba2bf2080e91f265e4cb37ef2b7e19d24
tribikram <sentribikram@gmail.com>
2024-08-29 23:25:39 +0545

    fix mobile view

--- src/components/common/MobileNavbar.jsx
+++ src/components/common/MobileNavbar.jsx
@@ -1,25 +1,25 @@
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
-// import { RxCross2 } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import PropTypes from "prop-types";
 
 const MobileNavbar = ({ navLinks, onClose, pathname }) => {
   return (
-    <div className="fixed transition-all delay-1000 md:hidden inset-0 bg-white z-50 flex flex-col items-center py-6 my-3 w-1/2">
-      <div className="flex justify-between w-full px-2 mb-10">
+    <div className="fixed transition-all delay-1000 md:hidden inset-0 bg-cardBackgroundLight z-50 flex flex-col  py-6 my-1 h-full">
+      <div className="flex justify-between items-center w-full px-2 mb-10">
         <Logo onClose={onClose} />
-        {/* <RxCross2
+        <RxCross2
           onClick={() => onClose(false)}
           className="text-xl sm:text-2xl"
-        /> */}
+        />
       </div>
 
-      <ul className="flex flex-col justify-start text-start  items-center space-y-4 transition-all delay-100">
+      <ul className="flex flex-col   space-y-4 transition-all delay-100">
         {navLinks?.map(({ href, label }) => (
           <Link
             key={href}
             to={href}
-            className={`text-[1rem] text-textDark  ${pathname === href ? 'opacity-100' : 'opacity-40'}`}
+            className={`px-2 text-[1rem] text-textDark  ${pathname === href ? 'opacity-100 font-medium bg-cardBackgroundDark py-2 rounded-sm' : 'opacity-40'}`}
             onClick={() => onClose(false)}>
             {label}
           </Link>
@@ -30,7 +30,7 @@ const MobileNavbar = ({ navLinks, onClose, pathname }) => {
       <div className="min-w-full h-px bg-[#E0E0E0] mt-[30px] px-4"></div>
 
       <div className="flex flex-row items-center space-x-4 mt-8">
-        <select className="bg-white  rounded-md p-2 text-[1rem]">
+        <select className="bg-cardBackgroundLight  rounded-md p-2 text-[1rem]">
           <option>USD</option>
         </select>
         <Link to='/listings' onClick={() => onClose(false)} className="flex p-[14px_20px] justify-center items-center gap-[10px] rounded-xl bg-[#222]">
