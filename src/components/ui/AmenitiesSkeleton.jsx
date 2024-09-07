59ee999277f7172ec207eaa70d770efbc57958a7
tribikram <sentribikram@gmail.com>
2024-09-07 22:14:51 +0545

    change ui to full width

--- src/components/ui/AmenitiesSkeleton.jsx
+++ src/components/ui/AmenitiesSkeleton.jsx
@@ -1,6 +1,6 @@
 const AmenitiesSkeleton = () => {
   return (
-    <div className="max-w-sm animate-pulse">
+    <div className="w-full animate-pulse">
       <div className="h-[60px] bg-textLight rounded-2xl max-w-[360px] mb-2.5"></div>
       <span className="sr-only">Loading...</span>
     </div>
