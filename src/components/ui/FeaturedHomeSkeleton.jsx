fa5b85c98d6a8c434789cb5a00dfcb0ae6acf5c4
tribikram <sentribikram@gmail.com>
2024-10-28 12:25:31 +0545

    color change for home skeleton

--- src/components/ui/FeaturedHomeSkeleton.jsx
+++ src/components/ui/FeaturedHomeSkeleton.jsx
@@ -9,28 +9,28 @@ const FeaturedHomeSkeleton = () => {
         <div className="flex flex-col justify-end space-y-10 sm:min-w-[400px]">
 
           <div className="flex flex-col space-y-4">
-            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-textLight rounded-lg">
+            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-cardBackgroundDark rounded-lg">
               <div className="text-gray-500 w-full h-6"></div>
             </div>
-            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-textLight rounded-lg">
+            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-cardBackgroundDark rounded-lg">
               <div className="text-gray-500 w-full h-6 "></div>
             </div>
-            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-textLight rounded-lg">
+            <div className="text-xl sm:text-2xl md:text-[28px] font-medium tracking-[-1%] lg:max-w-[365px] bg-cardBackgroundDark rounded-lg">
               <div className="text-gray-500 w-full h-6 "></div>
             </div>
           </div>
 
           <div className="flex flex-wrap gap-2 ">
-            <div className="flex gap-1 border bg-textLight border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
+            <div className="flex gap-1 border bg-cardBackgroundDark border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
             </div>
-            <div className="flex gap-1 border bg-textLight border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
+            <div className="flex gap-1 border bg-cardBackgroundDark border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
 
             </div>
-            <div className="flex gap-1  border bg-textLight border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
+            <div className="flex gap-1  border bg-cardBackgroundDark border-[#7B6944] px-10 py-1 items-center rounded-2xl text-xs sm:text-sm">
             </div>
           </div>
         </div>
-        <div className=" w-full h-[400px] lg:h-[500px] bg-textLight rounded-2xl">
+        <div className=" w-full h-[400px] lg:h-[500px] bg-cardBackgroundDark rounded-2xl">
         </div>
       </div>
     </Wrapper>
