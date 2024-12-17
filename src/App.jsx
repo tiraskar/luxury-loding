898922937697c81031b33a8c58d2c398d45374f3
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2024-12-17 19:16:23 +0545

    minor configuration changes

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ const router = createBrowserRouter([
   },
 ],
   {
-    basename: '',
+    basename: '/',
   }
 );
 
--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/',
   server: {
     historyApiFallback: true, // Ensures SPA routing is respected
   },
