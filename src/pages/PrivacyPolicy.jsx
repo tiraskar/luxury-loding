879780b07ab7f0d4df4954ff64612432086a4680
tribikram <sentribikram@gmail.com>
2025-03-10 13:17:17 +0545

    create privacy policy

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,148 @@
+import { Wrapper } from "../components";
+
+const PrivacyPolicy = () => {
+  return (
+    <Wrapper>
+      <div className='flex flex-col space-y-5 font-inter'>
+        <h1 className='text-2xl font-bold font-onest'>
+          Privacy Policy
+        </h1>
+        <p>
+          At Luxury Lodging Host, accessible at <strong>
+            http://www.luxurylodgingpm.co
+          </strong>, one of our main priorities is the privacy of our visitors.
+        </p>
+        <p>
+          This Privacy Policy document contains types of information that is collected and recorded by <strong>http://www.luxurylodgingpm.co</strong> and how we use it.
+        </p>
+        <p>
+          If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email at <strong>receipts@luxurylodgingstr.com</strong>.
+        </p>
+        <p>
+          This privacy policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collected in <strong>www.luxurylodgingpm.co</strong>
+        </p>
+        <p>
+          This policy is not applicable to any information collected offline or via channels other than this website.
+        </p>
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          Consent
+        </h2>
+        <p>
+          By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+        </p>
+
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          Information we collect
+        </h2>
+        <p>
+          The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.
+        </p>
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          How we use your information
+        </h2>
+        <p>
+          We use the information we collect in various ways, including to:
+        </p>
+        <ul className='list-disc pl-10'>
+          <li>
+            Provide, operate, and maintain our website
+          </li>
+          <li>
+            Improve, personalize, and expand our website
+          </li>
+          <li>
+            Understand and analyze how you use our website
+          </li>
+          <li>
+            Develop new products, services, features, and functionality
+          </li>
+          <li>
+            Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
+          </li>
+          <li>
+            Send you emails
+          </li>
+          <li>
+            Find and prevent fraud
+          </li>
+        </ul>
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          Log Files
+        </h2>
+        <p>
+          <strong>Luxury Lodging</strong> Host follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this as part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+        </p>
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          Cookies and Web Beacons
+        </h2>
+        <p>
+          Like any other website, <strong>Luxury Lodging</strong> Host uses ‘cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+        </p>
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          CCPA Privacy Policy (Do Not Sell My Personal Information)
+        </h2>
+        <p>
+          Under the CCPA, among other rights, California consumers have the right to:
+        </p>
+        <p>
+          Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.
+        </p>
+        <p>
+          Request that a business delete any personal data about the consumer that a business has collected.
+        </p>
+        <p>
+          Request that a business that sells a consumer's personal data, not sell the consumer's personal data.
+        </p>
+        <p>
+          If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+        </p>
+
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          GDPR Privacy Policy (Data Protection Rights)
+        </h2>
+
+        <p>
+          We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+        </p>
+        <p>
+          The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
+        </p>
+        <p>
+          The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.
+        </p>
+        <p>
+          The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
+        </p>
+        <p>
+          The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+        </p>
+        <p>
+          The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+        </p>
+        <p>
+          The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+        </p>
+        <p>
+          If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+        </p>
+
+        <h2 className='font-bold text-lg font-onest mt-0.5'>
+          Children's Information
+        </h2>
+
+        <p>
+          Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+        </p>
+
+        <p>
+          <strong>Luxury Lodging</strong> Host does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+        </p>
+
+
+      </div>
+
+    </Wrapper>
+  );
+};
+
+export default PrivacyPolicy;
