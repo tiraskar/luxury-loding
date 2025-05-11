ca8b12cce369488820531384117b472cb1be1470
tribikram <sentribikram@gmail.com>
2025-05-11 23:14:11 +0545

    remove strip load

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -1,6 +1,6 @@
-import { loadStripe } from "@stripe/stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+// export const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 export const appearance = {
   theme: 'flat',
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import App from './App.jsx'
 import './index.css';
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-calendar/dist/Calendar.css';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+// import { Elements } from '@stripe/react-stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
 
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css'
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+// const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Elements stripe={stripePromise} >
       <App />
-    </Elements>
+    {/* <Elements stripe={stripePromise} >
+    </Elements> */}
   </React.StrictMode>
 )
