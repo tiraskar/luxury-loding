2f8605873f855414508e52eeb038b5c53e2634a0
Tribikram Sen <sentribikram@gmail.com>
2025-07-09 20:55:04 +0545

    css import and google map load key

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,23 @@ import App from './App.jsx'
 import './index.css';
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-calendar/dist/Calendar.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 // import { Elements } from '@stripe/react-stripe-js';
 // import { loadStripe } from '@stripe/stripe-js';
 
+
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css'
+import { LoadScript } from '@react-google-maps/api';
 
 // const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
     {/* <Elements stripe={stripePromise} >
     </Elements> */}
+    <App />
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_KEY}>
+    </LoadScript>
   </React.StrictMode>
 )
